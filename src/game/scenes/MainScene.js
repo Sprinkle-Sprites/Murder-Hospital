2e4820bffd1a4db54cc20b16df0e6eae5c123316
Gabriella Apeadu <gabriellaapeadu@Gabriellas-MacBook-Pro.local>
2021-10-08 12:51:06 -0400

    sample board added

--- src/game/scenes/MainScene.js
+++ src/game/scenes/MainScene.js
@@ -1,10 +1,9 @@
-import { Scene } from "phaser";
+import Phaser, { Scene } from "phaser";
 import Player from "@/game/Player";
 
 class MainScene extends Scene {
   constructor() {
-    // super({ key: "MainScene" });
-    super();
+    super({ key: "MainScene" });
   }
 
   preload() {
@@ -13,6 +12,27 @@ class MainScene extends Scene {
 
   create() {
     this.createPlayer();
+    const map = this.make.tilemap({ key: "board" });
+    const tileset = map.addTilesetImage(
+      "Interior-Hospital-A",
+      "tiles",
+      16,
+      16,
+      0,
+      0
+    );
+
+    const wallsLayer = map.createLayer("WallsLayer", tileset);
+
+    wallsLayer.setCollisionByProperty({ collides: true });
+
+    const debugGraphics = this.add.graphics().setAlpha(0.7);
+
+    wallsLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+    });
   }
 
   update() {
