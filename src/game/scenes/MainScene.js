132b7ddaa1c117dfb8cd9904d94ce0a23c41ed72
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-10-11 17:48:16 -0400

    refactored main scene component/ clean up code

--- src/game/scenes/MainScene.js
+++ src/game/scenes/MainScene.js
@@ -11,7 +11,19 @@ class MainScene extends Scene {
   }
 
   create() {
+    this.createMap();
     this.createPlayer();
+  }
+
+  update() {
+    this.player.update();
+  }
+
+  resizeCollider(obj, num) {
+    obj.body.setSize(obj.width - num, obj.height - num, true);
+  }
+
+  createMap() {
     const map = this.make.tilemap({ key: "board" });
     const tileset = map.addTilesetImage(
       "Interior-Hospital-A",
@@ -22,12 +34,14 @@ class MainScene extends Scene {
       0
     );
 
+    //LAYERS
     const wallsLayer = map.createLayer("WallsLayer", tileset);
 
+    //LAYER COLLIDERS
     wallsLayer.setCollisionByProperty({ collides: true });
 
+    //COLLIDER DEBUG COLOR
     const debugGraphics = this.add.graphics().setAlpha(0.7);
-
     wallsLayer.renderDebug(debugGraphics, {
       tileColor: null,
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
@@ -35,14 +49,18 @@ class MainScene extends Scene {
     });
   }
 
-  update() {
-    this.player.update();
-  }
-
   createPlayer() {
     this.player = this.physics.add.existing(
       new Player(this, 250, 250, "player1")
     );
+
+    //ADJUSTS COLLIDER TO SURROUND PLAYER
+    this.time.addEvent({
+      delay: 100,
+      callback: () => this.resizeCollider(this.player, 20),
+      callbackScope: this,
+      loop: false,
+    });
   }
 }
 
