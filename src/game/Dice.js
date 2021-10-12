05b68ce3f52c3b05f77cda78e86e459b60025a57
Gabby Apeadu <apeadug@gmail.com>
2021-10-12 10:51:55 -0400

    dice rolls all numbers & console matches number on die

--- src/game/Dice.js
+++ src/game/Dice.js
@@ -1,5 +1,7 @@
-// load dice images
+//load image files
+
 let images = [
+  "dieRed1.png", // set first two array images to die image 1
   "dieRed1.png",
   "dieRed2.png",
   "dieRed3.png",
@@ -9,24 +11,23 @@ let images = [
   "dieRed7.png",
   "dieRed8.png",
 ];
-
-//return list of elements that include "img"
+// select all elements that have img
 let dice = document.querySelectorAll("img");
 
 function rollDie() {
-  //give dice shake property to simulate shaking die
+  // add a shake property to so that dice "shakes"
   dice.forEach((die) => {
     die.classList.add("shake");
   });
 
-  // have dice change after a second
   setTimeout(() => {
-    dice.forEach(function(die) {
+    dice.forEach((die) => {
       die.classList.remove("shake");
     });
-    let dieValue = Math.floor(Math.random() * 8);
 
-    //console.log(dieValue); //use die value to connect to room
+    //dieValue must be a number from 1 - 8
+    let dieValue = Math.floor(Math.random() * 8) + 1;
+    console.log(dieValue); //connect dieValue to room
 
     document
       .querySelector("#die-1")
@@ -36,5 +37,7 @@ function rollDie() {
 document
   .getElementById("investigate-btn")
   .addEventListener("click", rollDie, true);
+
+rollDie();
 
 export default rollDie;
