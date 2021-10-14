5281f230ff770609ce2cf38d34c2e139f90cbe47
Helena Bliss <helenajbliss75@gmail.com>
2021-10-14 16:05:50 -0500

    Still working on Multiplayer

--- src/game/assets/tiles/Locker Room.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Locker Room" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
- <image source="LockerRoom.png" width="768" height="768"/>
-</tileset>
