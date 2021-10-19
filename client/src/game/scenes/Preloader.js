a90e1910168667c97b1daff222b4da5ecfa05e17
Gabby Apeadu <apeadug@gmail.com>
2021-10-19 09:45:51 -0400

    morgue set up, need to add clues

--- client/src/game/scenes/Preloader.js
+++ client/src/game/scenes/Preloader.js
@@ -106,7 +106,7 @@ export default class Preloader extends Phaser.Scene {
     if (this.readyCount === 2) {
       // have it lead to first scene
       this.scene.start("MainTimerScene");
-      this.scene.start("Morgue");
+      this.scene.start("MainScene");
     }
   }
 }
