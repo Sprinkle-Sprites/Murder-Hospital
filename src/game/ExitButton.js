bd02edfb6bfedc6b422c49672763ca27b0a2ed68
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-10-20 12:06:46 -0400

    testing untracked

---
+++ src/game/ExitButton.js
@@ -0,0 +1,10 @@
+function exitButton() {
+  console.log("hello there")
+}
+
+document
+  .getElementById("try-to-leave")
+  .addEventListener("click", exitButton, true);
+
+  //this is a comment that I updated
+
