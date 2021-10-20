77cdcf8be27217aadf7878628dd5507bdf6cf1a5
Gabby Apeadu <apeadug@gmail.com>
2021-10-20 16:11:11 -0400

    play button starts game

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -26,8 +26,10 @@ import locker_room from "@/game/assets/tiles/LockerRoom.json";
 //GAME ASSETS
 import combination_code from "@/game/assets/popups/locker_combo.png";
 import lockerRoom from "@/game/assets/tiles/LockerRoom.png";
-import redButton from "@/game/assets/menuButtons/redBox.png";
-import redBorder from "@/game/assets/menuButtons/redBorder.png";
+import blueButton from "@/game/assets/menu/blue_button01.png";
+import redButton from "@/game/assets/menu/redBox.png";
+import redBorder from "@/game/assets/menu/redBorder.png";
+import hospital from "@/game/assets/menu/abandoned_Hospital.png";
 
 export default class Preloader extends Phaser.Scene {
   constructor() {
@@ -101,8 +103,10 @@ export default class Preloader extends Phaser.Scene {
 
     this.timedEvent = this.time.delayedCall(1000, this.readyScene, [], this);
     // load assets for title scene
+    this.load.image("blueButton1", blueButton);
     this.load.image("redButton", redButton);
     this.load.image("redBorder", redBorder);
+    this.load.image("hospital", hospital);
 
     // load any assets needed for game
     this.load.image("Interior-A", InteriorA);
@@ -134,9 +138,9 @@ export default class Preloader extends Phaser.Scene {
     //once readyCount is equal to 2, we know it is safe to start title scene
     if (this.readyCount === 2) {
       // have it lead to title scene
-      this.scene.start("Title");
-      this.scene.start("MainTimerScene");
-      this.scene.start("ClueBank");
+      this.scene.start("TitleScene");
+      // this.scene.start("MainTimerScene");
+      // this.scene.start("ClueBank");
     }
   }
 }
--- src/game/scenes/TitleScene.js
+++ src/game/scenes/TitleScene.js
@@ -3,20 +3,22 @@ import config from "../game";
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
-    super("Title");
+    super("TitleScene");
   }
 
   create() {
-    /************** PLAY BUTTON  *********************/
+    this.load.image(100, 100, "hospital");
+    this.createPlayButton();
+  }
+
+  createPlayButton() {
     // CREATE PLAY SPRITE AND SET INTERACTION
-    this.playButton = this.add.sprite(100, 200, "redButton").setInteractive();
+    this.playButton = this.add.sprite(500, 300, "blueButton1").setInteractive();
 
-    //center ui buttons
-    this.centerButton(this.playButton, 1);
-
-    this.gameText = this.add.text(0, 0, "Play", {
+    this.gameText = this.add.text(0, 0, "Enter", {
       fontSize: "32px",
       fill: "#fff",
+      color: "#fffff",
     });
 
     //center text inside of button
@@ -25,56 +27,10 @@ export default class TitleScene extends Phaser.Scene {
     this.playButton.on(
       "pointerdown",
       function(pointer) {
-        this.scene.start("game");
+        this.scene.start("MainScene");
+        this.scene.start("ClueBank");
+        this.scene.start("MainTimerScene");
       }.bind(this)
-    );
-
-    /******** end of play button  ***************/
-
-    // /********** options button  ***************/
-
-    // //set interactive options button
-    // this.optionsButton = this.add
-    //   .sprite(400, 200, "blueButton1")
-    //   .setInteractive();
-
-    // //center ui option button
-    // this.centerButton(this.playButton, 1);
-
-    // this.optionsText = this.add.text(0, 0, "Options", {
-    //   fontSize: "32px",
-    //   fill: "#fff",
-    // });
-
-    // this.centerButtonText(this.optionsText, this.optionsButton);
-
-    // this.optionsButton.on(
-    //   "pointerdown",
-    //   function(pointer) {
-    //     this.scene.start("Options");
-    //   }.bind(this)
-    // );
-
-    // this.input.on("pointerover", function(event, gameObjects) {
-    //   gameObjects[0].setTexture("blueButton2");
-    // });
-
-    // this.input.on("pointerout", function(event, gameObjects) {
-    //   gameObjects[0].setTexture("blueButton1");
-    // });
-
-    /******** end of options button  ***************/
-  } //end of create
-
-  centerButton(gameObject, offset = 0) {
-    Phaser.Display.Align.In.Center(
-      gameObject,
-      this.add.zone(
-        config.width / 2,
-        config.height / 2 - offset * 100,
-        config.width,
-        config.height
-      )
     );
   }
 
