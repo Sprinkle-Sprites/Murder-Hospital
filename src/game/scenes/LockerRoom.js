2ee7d7b6baea17e521d12a780e134efea9f6928f
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-10-21 12:39:54 -0400

    bug: fix locker combo confusion

--- src/game/scenes/LockerRoom.js
+++ src/game/scenes/LockerRoom.js
@@ -155,6 +155,14 @@ class LockerRoom extends Scene {
     });
     this.roomTimer = new RoomTimer(this, roomTimerLabel);
     this.roomTimer.start(this.handleRoomCountdownFinished.bind(this));
+
+    //     //COLLIDER DEBUG COLOR
+    // const debugGraphics = this.add.graphics().setAlpha(0.7);
+    // extra2Layer.renderDebug(debugGraphics, {
+    //   tileColor: null,
+    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+    // });
   }
 
   handleRoomCountdownFinished() {
